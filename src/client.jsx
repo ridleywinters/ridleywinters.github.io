70e5f9a1676cb022f88258f2dd308745751fc5ef
ridleywinters <ridleywinters@nospam.io>
2020-06-13 10:34:40 -0700

    Push from monorepo

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as base from './base/ui/mod.jsx';
 import Application from './views/application.jsx';
-
 
 ReactDOM.render(<Application />, document.getElementById("root"));
