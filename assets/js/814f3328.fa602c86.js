ce98d3ff161a49f463a8c4958e0a8f72fcdfbbfa
Ridley Winters <ridley.grenwood.winters@gmail.com>
2024-08-10 14:38:19 -0700

    Update website

--- assets/js/814f3328.fa602c86.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"TODO","permalink":"/blog/usability-rants","unlisted":false,"date":"2024-07-26T00:00:00.000Z"}]}')}}]);
